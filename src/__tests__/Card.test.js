0320a5f81687a7fbc05544dddce50c191f6af881
Marlon Sanchez <Trugrey@gmail.com>
2024-10-27 07:08:26 -0400

    experimenting with jest testing

---
+++ src/__tests__/Card.test.js
@@ -0,0 +1,7 @@
+// import Card from "../Card";
+
+// describe('create card Class', () => {
+//     it('creates cards with ranks,suits and number values', () => {
+//         expect(new Card('Q', '♥')).toEqual('12', '♥')
+//     })
+// })
