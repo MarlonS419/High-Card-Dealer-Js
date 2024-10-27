f82308237c77387adbbc515589f5043cc654d33b
Marlon Sanchez <Trugrey@gmail.com>
2024-10-27 06:48:09 -0400

    comments to understand the code better

--- src/Deck.js
+++ src/Deck.js
@@ -1,26 +1,33 @@
 import _ from 'lodash';
 import Card from './Card.js';
 
+// suits and ranks var.
 const suits = ['♦', '♣', '♠', '♥'];
 const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 
+// Deck class with no arg. in the constructor to be able to just run when called.
 class Deck {
   constructor() {
+    // calling upon the cards var. within the buildDeck method
     this.cards = this.buildDeck();
   }
-
+  // method to create deck
   buildDeck() {
+    // empty cards array with variability
     let cards = [];
+    // for each suit loop thru the ranks
     suits.forEach(suit => {
+      // for each rank push the new card into the deck
       ranks.forEach(rank => {
         cards.push(new Card(rank, suit));
       });
     });
+    // use lodash mod to  shuffle cards randomly
     return _.shuffle(cards);
   }
-
+  // method to deal the randomized cards in sets of 4
   deal() {
-    return this.cards.splice(0, 4); // Modify to deal 4 cards at a time
+    return this.cards.splice(0, 4);
   }
 }
 
--- src/Game.js
+++ src/Game.js
@@ -16,12 +16,14 @@ console.log(hand1.cards);
 console.log("player2 has")
 console.log(hand2.cards);
 
+// total hands value
 const hand1Value = hand1.value();
 const hand2Value = hand2.value();
 
 console.log(`Player 1's hand value: ${hand1Value}`);
 console.log(`Player 2's hand value: ${hand2Value}`);
 
+// judge results of game
 if (hand1Value === hand2Value) {
     console.log("Tie!");
 } else if (hand1Value > hand2Value) {
